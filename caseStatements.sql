6d180b57a1ad0026f3043e416fe618621e79f688
jon <jonlyeatman@gmail.com>
2022-02-13 14:46:10 -0500

    case EX

---
+++ caseStatements.sql
@@ -0,0 +1,38 @@
+case {condition}
+  when {value1} then {result1}
+  when {value2} then {result2}
+  else {result3}
+end
+
+select
+  case name
+    when 'kale-smoothie'    then 'smoothie'
+    when 'banana-smoothie'  then 'smoothie'
+    when 'orange-juice'     then 'drink'
+    when 'soda'             then 'drink'
+    when 'blt'              then 'sandwich'
+    when 'grilled-cheese'   then 'sandwich'
+    when 'tikka-masala'     then 'dinner'
+    when 'chicken-parm'     then 'dinner'
+     else 'other'
+  end as category
+from order_items
+order by id
+limit 100;
+
+select
+  case name
+    when 'kale-smoothie'    then 'smoothie'
+    when 'banana-smoothie'  then 'smoothie'
+    when 'orange-juice'     then 'drink'
+    when 'soda'             then 'drink'
+    when 'blt'              then 'sandwich'
+    when 'grilled-cheese'   then 'sandwich'
+    when 'tikka-masala'     then 'dinner'
+    when 'chicken-parm'     then 'dinner'
+    else 'other'
+  end as category, round(1.0 * sum(amount_paid) /
+    (select sum(amount_paid) from order_items) * 100, 2) as pct
+from order_items
+group by 1
+order by 2 desc;
